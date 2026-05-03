c2e7610089f71c7d6953ee42be8b33df2e19c7c3
Aki-Hiro Sato <122781546+aki-cell2366@users.noreply.github.com>
2026-05-03 12:59:54 +0900

    Update comments on standards.

--- worldmesh.js
+++ worldmesh.js
@@ -7,8 +7,11 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
 // javascript fnctions to calculate the world grid square code.
-// The world grid square code computed by this library is
-// compatible to JIS X0410.
+// The world grid square code computed by this library 
+// is compatible to Japanese grid square code (JIS X 0410). 
+//
+// This library for World grid square code complies ISO 24108-1:
+// https://www.iso.org/standard/87706.html
 //
 // Version 1.0: Released on 2 February 2017
 // Version 1.01: Released on 6 February 2017
