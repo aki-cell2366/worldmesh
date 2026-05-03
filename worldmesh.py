dd5a126cd0f5739b9057dafd3a00288e653acfb8
Aki-Hiro Sato <122781546+aki-cell2366@users.noreply.github.com>
2026-05-03 13:00:54 +0900

    Clarify comments regarding grid square code compatibility
    Updated comments to clarify compatibility with Japanese grid square code and added compliance information for ISO 24108-1.

--- worldmesh.py
+++ worldmesh.py
@@ -7,8 +7,11 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #
 # Python functions to calculate the world grid square code.
-# The world grid square code computed by this library
-# is compatible to JIS X 0410.
+# The world grid square code computed by this library 
+# is compatible to Japanese grid square code (JIS X 0410). 
+#
+# This library for World grid square code complies ISO 24108-1:
+# https://www.iso.org/standard/87706.html
 #
 # Release notes
 # Version 1.0 : Released on 4 April 2017
